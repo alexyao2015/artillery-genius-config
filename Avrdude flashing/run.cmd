79b625246720b9e79e469354c8d56ae4e9d0fe60
Alex <33379584+alexyao2015@users.noreply.github.com>
2021-12-13 00:33:45 -0600

    Fix command

--- Avrdude flashing/run.cmd
+++ Avrdude flashing/run.cmd
@@ -1,3 +1,3 @@
 rem Can add -P COM13 to manually specify com port
-avrdude -p ATMEGA2560 -c arduino -V -U flash:w:"firmware.hex":i
+avrdude -p ATMEGA2560 -P COM12 -c wiring -V -U flash:w:"firmware.hex":i -D
 pause
